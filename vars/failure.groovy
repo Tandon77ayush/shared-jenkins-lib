e66746e212474f572fb5a63ba47537c168daf5e0
Ayush <139102333+Tandon77ayush@users.noreply.github.com>
2026-06-03 10:32:56 +0530

    Delete vars/failure.groovy

--- vars/failure.groovy
+++
@@ -1,8 +0,0 @@
-def call(String sub, String to, string msg){
-                emailext(
-                         subject: '$sub',
-                         to: '$to',
-                         body: '$msg'
-                )
-  
-}
